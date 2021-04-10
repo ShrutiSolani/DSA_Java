dfad23ee92cb8ded227ac5005771bc9d72026748
ShrutiSolani <shrutiusolani@gmail.com>
2021-04-10 20:29:32 +0530

    3. Kth Smallest Element

---
+++ Basic/KSmallest.java
@@ -0,0 +1,43 @@
+// { Driver Code Starts
+//Initial Template for Java
+
+/*package whatever //do not write package name here */
+
+import java.io.*;
+import java.util.*;
+class KSmallest {
+	public static void main (String[] args) {
+		Scanner sc=new Scanner(System.in);
+		
+		int t=sc.nextInt();
+		
+		while(t-->0)
+		{
+		    int n=sc.nextInt();
+		    
+		    int arr[]=new int[n];
+		    
+		    for(int i=0;i<n;i++)
+		    arr[i]=sc.nextInt();
+		    
+		    int k=sc.nextInt();
+		    Solution ob = new Solution();
+		    System.out.println(ob.kthSmallest(arr, 0, n-1, k));
+		}
+	}
+}
+// } Driver Code Ends
+
+
+//User function Template for Java
+
+class Solution{
+    public static int kthSmallest(int[] arr, int l, int r, int k) 
+    { 
+        Arrays.sort(arr);
+        return arr[k-1];
+        //Your code here
+        
+    } 
+   
+}
