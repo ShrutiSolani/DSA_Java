88b844823b20f47b5556cecd439739d07b02d419
ShrutiSolani <68545365+ShrutiSolani@users.noreply.github.com>
2021-08-16 10:04:21 +0530

    Create RemoveElement.java

---
+++ Array/RemoveElement.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int removeElement(int[] nums, int val) {
+        int count = 0;
+        int i;
+        for(i = 0;i < nums.length; i++){
+            if(nums[i] != val){
+                nums[count] = nums[i];
+                count++;
+            }
+        }
+        return count;     
+    }
+}
