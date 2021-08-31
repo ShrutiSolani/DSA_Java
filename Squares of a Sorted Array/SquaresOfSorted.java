b00fa414a0a7709b4f0ef17bc35a0234ea6d4ff6
ShrutiSolani <68545365+ShrutiSolani@users.noreply.github.com>
2021-08-31 09:36:49 +0530

    Update SquaresOfSorted.java

--- Squares of a Sorted Array/SquaresOfSorted.java
+++ Squares of a Sorted Array/SquaresOfSorted.java
@@ -1,1 +1,18 @@
-
+class Solution {
+    public int[] sortedSquares(int[] nums) {
+        int squares[] = new int[nums.length];
+        int low = 0, high = nums.length-1, i = high;
+        while(low <= high){
+            if(Math.abs(nums[low]) > Math.abs(nums[high])){
+                squares[i] = nums[low]*nums[low];
+                low += 1;
+            }
+            else{
+                squares[i] = nums[high]*nums[high];
+                high -= 1;
+            }
+            i-= 1;
+        }
+        return squares;
+    }
+}
