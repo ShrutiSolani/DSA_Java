f0c01ac1042d2d69c65069f74c5586c62f104fcd
ShrutiSolani <68545365+ShrutiSolani@users.noreply.github.com>
2021-09-16 19:42:33 +0530

    Create NumberOfPaths.java

---
+++ Recursion/NumberOfPaths.java
@@ -0,0 +1,12 @@
+class Solution{
+    
+    long numberOfPaths(int m, int n) {
+        // Code Here
+        if(m == 1 || n == 1){
+            return 1;
+        }
+        
+        return numberOfPaths(m-1,n)+numberOfPaths(m,n-1);
+    }
+    
+}
