963426ca77dfc8f354bad1dd37ac13824b3ad2a2
Shruti Solani <68545365+ShrutiSolani@users.noreply.github.com>
2022-09-03 11:37:45 +0530

    Update Solution.java

--- Kadane's Algorithm/3. Maximum subarray sum after K concatenation/Solution.java
+++ Kadane's Algorithm/3. Maximum subarray sum after K concatenation/Solution.java
@@ -1,52 +1,42 @@
-import java.util.ArrayList;
-import java.util.Collections;
-
+import java.util.*;
 public class Solution {
-	
-    public static long kadane(ArrayList<Integer> arr){
-    	long curr = 0;
-        long max = 0;
-        int negative = 0;
-        for(int i = 0;i < arr.size(); i++){
-            if(arr.get(i) < 0){
-                negative ++;
-            }
-            curr = curr + arr.get(i);
-            max = Math.max(max, curr);
-            if(curr < 0){
-                curr = 0;
+    public static long kadaneOfOne(ArrayList<Integer> arr, int n){
+        long currentSum = 0;
+        long maxSum = Long.MIN_VALUE;
+
+        for(int i = 0;i < n;i++){
+            int x = arr.get(i);
+            currentSum += x;
+            maxSum = Math.max(currentSum, maxSum);
+
+            if(currentSum < 0){
+                currentSum = 0;
             }
         }
-        if(negative == arr.size()){
-            long x = Collections.max(arr);
-            return x;
-        }
-        return max;
+
+        return maxSum;
     }
+    
     
 	public static long maxSubSumKConcat(ArrayList<Integer> arr, int n, int k) {
 		// Write your code here.
         if(k == 1){
-            long response = kadane(arr);
-            return response;
+            return kadaneOfOne(arr, n);
         }
         else{
-            int sum = 0;
-            for (int number : arr){
-                sum += number;
+            long sumOfArray = 0;
+            for(int i = 0;i < arr.size();i++){
+                sumOfArray += arr.get(i);
             }
-            ArrayList<Integer> new_arr = new ArrayList<Integer>();
-        	new_arr.addAll(arr);
-            new_arr.addAll(arr);
-            if(sum < 0){    
-        		long response = kadane(new_arr);
-        		return response;
+            
+            arr.addAll(arr);
+            if(sumOfArray < 0){
+                return kadaneOfOne(arr, n*2);
             }
             else{
-                long response = kadane(new_arr);
-                response += (long)(k-2)*sum;
-                return response;
+                return (k-2)*sumOfArray + kadaneOfOne(arr, n*2);
             }
         }
+
 	}
 }
