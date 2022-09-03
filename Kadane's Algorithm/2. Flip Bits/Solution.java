d608894ae68b3b3693eadbdcabfa3e1385840e8e
Shruti Solani <68545365+ShrutiSolani@users.noreply.github.com>
2022-09-03 11:37:09 +0530

    Update Solution.java

--- Kadane's Algorithm/2. Flip Bits/Solution.java
+++ Kadane's Algorithm/2. Flip Bits/Solution.java
@@ -1,19 +1,30 @@
 public class Solution {
 	public static int flipBits(int[] arr,int n) {
         //Write your code here
+        
+        int current0 = 0;
+        int max0 = Integer.MIN_VALUE;
         int count1 = 0;
-        int curr = 0;
-        int max = 0;
-        for(int i = 0;i < n;i++){
-            if(arr[i] == 1){
+//         on flipping 1 -> 0 we loose 1, therfore val = -1 if we encounter 1 while traversing
+        
+//         on flipping 0 -> 1 we get 1, therefore val = 1 if we encounter 0 while traversing
+        
+        for(int num : arr){
+            int val = 1;
+            if(num == 1){
                 count1++;
-                curr = Math.max(-1, curr-1);
+                val = -1;
             }
-            else{
-                curr = Math.max(1, curr+1);
-            }
-            max = Math.max(curr, max);
+            
+            // we can either consider taking only this element or keeping it as part of previous subarray
+            current0 = Math.max(val, current0+val);
+            max0 = Math.max(current0, max0);
         }
-        return count1+max;
+        
+        if(max0 < 0){
+// if no 0->1 transformation needed, let max0 = 0
+            max0 = 0;
+        }
+        return max0+count1;
 	}
 }
