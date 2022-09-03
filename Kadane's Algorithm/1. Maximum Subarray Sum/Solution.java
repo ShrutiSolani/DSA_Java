e478ae578f81be5da898276e5bac4cb6a08f8baa
Shruti Solani <68545365+ShrutiSolani@users.noreply.github.com>
2022-09-03 11:36:32 +0530

    Update Solution.java

--- Kadane's Algorithm/1. Maximum Subarray Sum/Solution.java
+++ Kadane's Algorithm/1. Maximum Subarray Sum/Solution.java
@@ -1,23 +1,21 @@
+import java.util.* ;
+import java.io.*; 
 
 public class Solution {
 	
 	public static long maxSubarraySum(int[] arr, int n) {
-		// write your code here
-        if(arr.length == 0){
-            return (long)0;
-        }
-        else{
-            long currSum = 0;
-            long maxSum = 0;
-            for(int i = 0;i < arr.length;i++){
-                currSum = currSum + (long)arr[i];
-                maxSum = Math.max(currSum, maxSum);
-                if(currSum < 0){
-                    currSum = (long)0;
-                }
-            }
-            return maxSum;
-        }
+		long currentSum = 0;
+		long maxSum = Long.MIN_VALUE;
+
+		for(int num : arr){
+		    currentSum = currentSum + num;
+		    maxSum = Math.max(maxSum, currentSum);
+		    if(currentSum < 0){
+			currentSum = 0;
+		    }
+		}
+
+		return maxSum <= 0 ? 0 : maxSum;
 	}
 
 }
